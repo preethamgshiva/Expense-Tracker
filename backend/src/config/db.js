860970d32b499455e7ad40ed2b6038232f041d2e
preethamgshiva <preethamgshiva2004@gmail.com>
2025-12-02 12:05:07 +0530

    Fix db connection for serverless

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
+
+
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        isConnected = conn.connections[0].readyState;
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        // Do not exit process in serverless
     }
 };
 
